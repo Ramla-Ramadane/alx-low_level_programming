4c15b4372a7ab453c86f9b16474306b63b9d6764
Ramla-Ramadane <ramlaramadane@gmail.com>
2023-09-09 10:26:18 -0700

    print alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print characters in reverse
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	for (r = 'z' ; r >= 'a' ; r--)
+	{
+		putchar(r);
+	}
+
+	putchar('\n');
+	return (0);
+}
