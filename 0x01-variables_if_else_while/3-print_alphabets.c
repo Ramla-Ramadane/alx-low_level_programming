0ab7735d4fbcecfe74847782fefcc3ea991fb06c
Ramla-Ramadane <ramlaramadane@gmail.com>
2023-09-09 08:04:28 -0700

    print character in uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print characters in uppercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A' ; c <= 'Z' ; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
