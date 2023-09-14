86a353ca290f39a69e294e019c66c145dacefcec
Ramla-Ramadane <ramlaramadane@gmail.com>
2023-09-14 13:21:10 -0700

    nested loops task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checks for uppercase character
+ * @c: parameter to be checked
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	if (c == 'a')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+
+		return (0);
+}
