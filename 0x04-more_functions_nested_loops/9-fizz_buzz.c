84d29723a086f971edbc5a44ba65ded8e5ad77f5
Ramla-Ramadane <ramlaramadane@gmail.com>
2023-09-15 16:09:57 -0700

    nested loops task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * The “Fizz-Buzz test”
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1 ; a <= 100; a++)
+	{
+		if (a % 5 == 0 && a % 3 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			printf("%i", a);
+		}
+			printf(" ");
+	}
+
+			printf("\n");
+			return (0);
+}
