f38d37cd9d7cd9a506d44ac06fb170dbd0d9e56d
Ramla-Ramadane <ramlaramadane@gmail.com>
2023-09-18 13:25:45 -0700

    pointers task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - function that takes a pointer to an int as parameter and updates the value it points to to 98
- * @*n: parameter to be checked
+ * reset_to_98 - function takes a pointer updates the value it points to 98
+ * @n: parameter to be checked
  * Return: void
  */
 void reset_to_98(int *n)
