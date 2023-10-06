b04b9ba7b87c45935b033dbd845810e163c1eadb
Ramla-Ramadane <ramlaramadane@gmail.com>
2023-10-06 15:22:02 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: arguments content
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int c;
+
+	for (c = 0; c < argc; c++)
+	{
+		printf("%s\n", argv[c]);
+	}
+
+		return (0);
+}
