c966e758eba0230fe02b7e1282036b6f16bb99b6
Ramla-Ramadane <ramlaramadane@gmail.com>
2023-10-12 09:52:04 -0700

    function that prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,16 +18,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		return;
 	}
+	if (separator == NULL)
+	{
+		;
+	}
 	else
 	{
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", va_arg(number, int));
-			if (separator == NULL)
-			{
-				;
-			}
-			else if (i < (n - 1))
+			if (i < (n - 1))
 			{
 				printf("%s", separator);
 			}
