45d0ca57d111611dcefc40c0dd3f1b5b8c05a126
Ramla-Ramadane <ramlaramadane@gmail.com>
2023-10-23 13:09:42 -0700

    function that frees a linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: points to the first node
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temporary;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	while (*head != NULL)
+	{
+		temporary = (*head)->next;
+		free(*head);
+		(*head) = temporary;
+	}
+	*head = NULL;
+}
+
