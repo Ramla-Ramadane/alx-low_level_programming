5cd7d97d97fa0290ea585a5522d878eab99b39ed
Ramla-Ramadane <ramlaramadane@gmail.com>
2023-10-24 09:06:55 -0700

    function reverses linked list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,7 +11,7 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *curr = *head;
 	listint_t *next = NULL;
 
-	while (curr != NULL)
+	while (*head)
 	{
 		next = curr->next;
 		curr->next = prv;
