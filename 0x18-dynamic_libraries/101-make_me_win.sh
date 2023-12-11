a83a1cfc65b644a8d6cad964447a036850ae3e61
Ramla-Ramadane <ramlaramadane@gmail.com>
2023-12-11 09:02:23 -0800

    alx task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Ramla-Ramadane/alx-low_level_programming/raw/master/0x18-dynamic_libraries/iwin.so
-export LD_PRELOAD=/tmp/iwin.so
+wget -P /tmp https://github.com/Ramla-Ramadane/alx-low_level_programming/raw/master/0x18-dynamic_libraries/preload.so
+export LD_PRELOAD=/tmp/preload.so
