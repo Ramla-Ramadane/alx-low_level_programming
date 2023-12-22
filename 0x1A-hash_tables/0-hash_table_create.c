9435c10bdfb5d8fe6ccdf9a910e4e7706f8b8b23
Ramla-Ramadane <ramlaramadane@gmail.com>
2023-12-22 08:47:51 -0800

    function that creates a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create -  function that creates a hash table
+ * @size: is the size of the array
+ * Return: if success a pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int count = 0;
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while(count < size)
+		{
+			ht->array[count] = NULL;
+			count++;
+		}
+	}
+	return (ht);
+}
